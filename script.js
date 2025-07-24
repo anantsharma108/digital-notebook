dd50673cc85ab3b51a456b89cccfc5e290e9b381
anantsharma108 <anantwwwww@gmail.com>
2025-07-24 18:43:55 +0530

    blueprint

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ let pages=document.querySelectorAll('.page')
 let url="https://api.adviceslip.com/advice"
 let advice=document.querySelector('.advice p')
 
-async function thought(){
+async function read(){
     let response= await fetch(url);
     let data= await response.json()
     return data.slip.advice
 }
 
-
+function modify(){
+    const urlParams=new URLSearchParams(window.location.search);
+    const isAuthenticated=urlParams.get('authenticated');
+    if (isAuthenticated){
+        page2=document.querySelector('.page2')
+        page2.innerText='Welcome dosto'
+    }
+}
 
 menu_bar.style.visibility='hidden'
 menu_icons.forEach((menu_icon)=>{
     menu_icon.addEventListener('click',async ()=>{
     if(menu_bar.style.visibility==='hidden'){
         menu_bar.style.visibility='visible'
-        text=await thought()
-        advice.innerText=text
+        advice.innerText=await read()
     }else{
         menu_bar.style.visibility='hidden'
     }
@@ -35,3 +41,5 @@ options.forEach((option)=>{
         })
     })
 })
+
+modify()
